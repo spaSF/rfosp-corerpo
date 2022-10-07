8a69fe0a4c10fe2330d02443dc5c8a36b292e6f1
Lukas Bacigal <lbacigal@gratex.com>
2022-10-07 15:32:00 +0200

    add logging

--- rpo/src/main/java/com/gratex/helloworld/HelloController.java
+++ rpo/src/main/java/com/gratex/helloworld/HelloController.java
@@ -4,15 +4,22 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
+
+    private static Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+
     @Value("${hello.message:world}")
     private String message;
 
     @GetMapping
     public String hello() {
+        logger.info("Hello controller called.");
         return "Hello "+ message;
     }
 }
